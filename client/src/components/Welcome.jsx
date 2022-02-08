5692f61068c92bbcf1f3db0175f75ee1b17af42b
Mark <markchristygeorge@gmail.com>
2022-02-08 20:52:00 +0100

    responsive design fixes

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -65,7 +65,7 @@ const Welcome = () => {
                 </div>
 
                 <div className="flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10">
-                    <div className="p-2 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmohism">
+                    <div className="p-2 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-64 my-5 eth-card white-glassmohism">
                         <div className="flex justify-between flex-col w-full h-full">
                             <div className="flex justify-between items-start">
                                 <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center">
